82e422274887f636f3fc6f35e7e9ba44c7638703
Ali Nugen <alexandra.nugen@gmail.com>
2022-10-22 23:03:59 -0700

    updating for debugging

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,8 +3,8 @@ const {Thought, Reaction, User} = require('../models');
 const thoughtController = {
     // Get all Thoughts
     getThoughts(req, res) {
-        Thought.find({})
-            .select('-__v')
+        Thought.find()
+            .select("-__v")
             .then((thoughtData) => res.json(thoughtData))
             .catch((err) => res.status(500).json(err));
     },
